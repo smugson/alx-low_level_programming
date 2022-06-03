2ac770c1c073aac713d9d57293fc36b5d059bf10
smugson <smugson499@gmail.com>
2022-06-02 17:52:01 -0700

    should work

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  * Return: Always 0 (Success)
@@ -9,7 +10,7 @@ int main(void)
 	long int b;
 	long long int c;
 	char d;
-	float f;\
+	float f;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));

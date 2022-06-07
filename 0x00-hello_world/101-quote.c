423726031667c4ec8cbc20adfbe54b75e6f8bcd3
smugson <smugson499@gmail.com>
2022-06-07 05:08:28 -0700

    correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,15 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point
- * Return: Always 1 (Success)
- */
+ *
+ *  * main - Entry point
+ *
+ *   * Return: Always 1 (Success)
+ *
+ *    */
 int main(void)
 {
-
-	fwrite(2, "and that piece of art is useful \" - Dora Korpar, 2015-10-19\n", 59);
+	fwrite(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
